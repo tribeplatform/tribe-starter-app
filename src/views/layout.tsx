caf5f918174e63b629086f67b5a7718e07c7589d
Ermia <qasemi.ermia@gmail.com>
2022-04-04 16:25:09 -0400

    Fix Invalid DOM property

--- src/views/layout.tsx
+++ src/views/layout.tsx
@@ -4,7 +4,7 @@ export default function AppLayout(props) {
   return <>
     <html lang="">
     <head>
-        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
         <meta charSet='utf-8' />
         <title>Welcome to Starter App</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
