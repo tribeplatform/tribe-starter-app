7f25e7c52ed472fd6a2d6ab8b36a6e6d98ed017b
Amir Ahangari <amir@ahangari.org>
2022-04-12 16:15:29 +0400

    Add handlers for GET_SETTINGS, UPDATE_SETTINGS and SUBSCRIPTION webhooks

--- src/controllers/webhook.controller.ts
+++ src/controllers/webhook.controller.ts
@@ -1,28 +1,105 @@
 import { NextFunction, Request, Response } from 'express';
 
-import ClientService from '@services/app.service';
+import { Types } from '@tribeplatform/gql-client';
+import { logger } from '@/utils/logger';
+
+const DEFAULT_SETTINGS = {};
 
 class WebhookController {
-  private clientService = new ClientService();
-
   public index = async (req: Request, res: Response, next: NextFunction) => {
+    const input = req.body;
     try {
-      if (req.body?.data?.challenge) {
-        res.json({
+      if (input.data?.challenge) {
+        return res.json({
           type: 'TEST',
           status: 'SUCCEEDED',
           data: {
             challenge: req.body?.data?.challenge,
           },
         });
-      } else {
-        const memberInfo = await this.clientService.getMemberByID(req.body.data.object.ownerId);
-        res.json(memberInfo);
       }
+      let result = {
+        type: input.type,
+        status: 'SUCCEEDED',
+        data: {},
+      };
+
+      switch (input.type) {
+        case 'GET_SETTINGS':
+          result = await this.getSettings(input);
+          break;
+        case 'UPDATE_SETTINGS':
+          result = await this.updateSettings(input);
+          break;
+        case 'SUBSCRIPTION':
+          result = await this.handleSubscription(input);
+          break;
+      }
+      res.status(200).json(result);
     } catch (error) {
-      next(error);
+      logger.error(error);
+      return {
+        type: input.type,
+        status: 'FAILED',
+        data: {},
+      };
     }
   };
+
+  /**
+   *
+   * @param input
+   * @returns { type: input.type, status: 'SUCCEEDED', data: {} }
+   * TODO: Elaborate on this function
+   */
+  private async getSettings(input) {
+    const currentSettings = input.currentSettings[0]?.settings || {};
+    let defaultSettings;
+    switch (input.context) {
+      case Types.PermissionContext.NETWORK:
+        defaultSettings = DEFAULT_SETTINGS;
+        break;
+      default:
+        defaultSettings = {};
+    }
+    const settings = {
+      ...defaultSettings,
+      ...currentSettings,
+    };
+    return {
+      type: input.type,
+      status: 'SUCCEEDED',
+      data: settings,
+    };
+  }
+
+  /**
+   *
+   * @param input
+   * @returns { type: input.type, status: 'SUCCEEDED', data: {} }
+   * TODO: Elaborate on this function
+   */
+  private async updateSettings(input) {
+    return {
+      type: input.type,
+      status: 'SUCCEEDED',
+      data: { toStore: input.data.settings },
+    };
+  }
+
+  /**
+   *
+   * @param input
+   * @returns { type: input.type, status: 'SUCCEEDED', data: {} }
+   * TODO: Elaborate on this function
+   */
+  private async handleSubscription(input) {
+    return {
+      type: input.type,
+      status: 'SUCCEEDED',
+      data: {},
+    };
+  }
 }
 
 export default WebhookController;
