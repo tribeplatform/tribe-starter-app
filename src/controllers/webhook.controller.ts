d2b6edf596c69c05aba58ac7f6690babcb8ff573
Amir Ahangari <amir@ahangari.org>
2022-04-12 16:25:44 +0400

    Add getSettings, updateSettings and handleSubscription functions to webhook controller

--- src/controllers/webhook.controller.ts
+++ src/controllers/webhook.controller.ts
@@ -1,5 +1,6 @@
 import { NextFunction, Request, Response } from 'express';
 
+import MixpanelService from '@/services/mixpanel.service';
 import { Types } from '@tribeplatform/gql-client';
 import { logger } from '@/utils/logger';
 
@@ -18,7 +19,7 @@ class WebhookController {
           },
         });
       }
-      let result = {
+      let result: any = {
         type: input.type,
         status: 'SUCCEEDED',
         data: {},
@@ -80,6 +81,22 @@ class WebhookController {
    * TODO: Elaborate on this function
    */
   private async updateSettings(input) {
+    if (!input.data.settings?.apiKey) {
+      return {
+        type: input.type,
+        status: 'FAILED',
+        errorCode: 112,
+        errorMessage: `Missing required parameter API key.`,
+      };
+    }
+    if (!input.data.settings?.region) {
+      return {
+        type: input.type,
+        status: 'FAILED',
+        errorCode: 112,
+        errorMessage: `Missing required parameter region.`,
+      };
+    }
     return {
       type: input.type,
       status: 'SUCCEEDED',
@@ -94,10 +111,80 @@ class WebhookController {
    * TODO: Elaborate on this function
    */
   private async handleSubscription(input) {
+    const networkSettings = input?.currentSettings.find(s => (s.context = Types.PermissionContext.NETWORK));
+    const apiKey = networkSettings?.settings.apiKey;
+    const region = networkSettings?.settings.region || 'us';
+    const data = input?.data || {};
+
+    if (apiKey && data.shortDescription) {
+      const { shortDescription, actor, time } = data;
+      const client = new MixpanelService(apiKey, region);
+
+      const event = {
+        event: shortDescription,
+        properties: {
+          time: new Date(time),
+          ...this.createEventProperties(data),
+          ...this.createMemberProperties(data),
+          distinct_id: actor?.id,
+        },
+      };
+
+      const result: any = client.track(event);
+
+      if (result?.status != 1) {
+        logger.error('Cannot send data to Mixpanel: ', {
+          status: result.status,
+          error: result.error,
+        });
+        return {
+          type: input.type,
+          status: 'FAILED',
+          data: result,
+        };
+      }
+      return {
+        type: input.type,
+        status: 'SUCCEEDED',
+        data: result,
+      };
+    }
+
     return {
       type: input.type,
       status: 'SUCCEEDED',
       data: {},
+    };
+  }
+
+  private createEventProperties(data): any {
+    if (!data?.object?.id) return {};
+    return {
+      ID: data.object.id,
+      Slug: data.object.slug,
+      Name: data.object.name,
+      Title: data.object.title,
+      Status: data.object.status,
+      'Created At': data.object.createdAt,
+      'Updated At': data.object.updatedAt,
+      'Created By ID': data.object.createdById,
+      'Owner ID': data.object.ownerId,
+      'Is Reply': data.object.isReply,
+      Count: data.object.count,
+      'Post ID': data.object.postId,
+      Reaction: data.object.reaction,
+      'Space ID': data.object.spaceId,
+      'Member ID': data.object.memberId,
+      'Inviter ID': data.object.inviterId,
+      Private: data.object.private,
+      Hidden: data.object.hidden,
+    };
+  }
+  createMemberProperties(data): any {
+    if (!data?.actor?.id) return {};
+    return {
+      'Member ID': data?.actor?.id,
+      'Role Type': data?.actor?.roleType,
     };
   }
 }
