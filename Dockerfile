1150b0364b5400b6d9a06d1692024453b9bd0286
Ermia <qasemi.ermia@gmail.com>
2022-04-13 12:55:21 -0400

    Fix Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -7,7 +7,7 @@ RUN yarn install --frozen-lockfile
 
 COPY . .
 
-RUN yarn build
+RUN yarn webpack:build
 
 FROM node:14.15.0-alpine AS Runner
 WORKDIR /tribe/app
@@ -22,8 +22,8 @@ RUN chown -R tribe:nodejs /tribe/app
 
 COPY --from=Builder /tribe/app/node_modules /tribe/app/node_modules
 COPY --from=Builder /tribe/app/dist /tribe/app/dist
+COPY --from=Builder /tribe/app/public /tribe/app/public
 COPY --from=Builder /tribe/app/package.json /tribe/app
 
 EXPOSE 3000
-
 CMD ["yarn", "start"]
--- server/src/config/index.ts
+++ server/src/config/index.ts
@@ -3,5 +3,17 @@ import { config } from 'dotenv';
 config({ path: `.env` });
 
 export const CREDENTIALS = process.env.CREDENTIALS === 'true';
-export const { NODE_ENV, PORT, DB_HOST, CLIENT_ID, CLIENT_SECRET, SIGNING_SECRET, NETWORK_ID, MEMBER_ID, GRAPHQL_URL, LOG_FORMAT, LOG_DIR, ORIGIN } =
-  process.env;
+export const {
+  NODE_ENV,
+  PORT,
+  DB_HOST,
+  CLIENT_ID,
+  CLIENT_SECRET,
+  SIGNING_SECRET,
+  NETWORK_ID,
+  MEMBER_ID,
+  GRAPHQL_URL,
+  LOG_FORMAT,
+  LOG_DIR = '../logs',
+  ORIGIN,
+} = process.env;
