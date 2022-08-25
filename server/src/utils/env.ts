e58d117de2e8145b16825dfce56fb0e847f8a9a8
Amir Ahangari <amir@ahangari.org>
2022-08-25 17:04:57 -0400

    fix env validation

--- server/src/utils/env.ts
+++ server/src/utils/env.ts
@@ -1,18 +1,18 @@
 import { config } from 'dotenv';
 import Joi from 'joi';
-import { logger } from '@utils/logger';
 
 config({ path: `.env` });
 
-const schema = Joi.object({
+const schema = {
   NODE_ENV: Joi.string().required(),
   PORT: Joi.number().required(),
   LOG_FORMAT: Joi.string().valid('debug', 'info', 'warn', 'error').default('info'),
   LOGGER_PRETTY_PRINT: Joi.boolean().default(false),
-});
-logger.log('Validating env...');
+};
 
-const result = schema.validate(process.env);
+const envValues = Object.fromEntries(Object.keys(schema).map(key => [key, process?.env[key]]))
+const result = Joi.object(schema).validate(envValues);
+console.log(result)
 if (result.error) {
   throw new Error(`Environment variables validation failed: ${result?.error?.message}`);
 }
